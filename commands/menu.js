1a54aecacf7ef62e9545e9ff00390a5e4673ae06
syuhada05 <addastorefollower@gmail.com>
2025-06-29 23:57:06 +0800

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,1 +1,32 @@
+module.exports = {
+  name: 'menu',
+  description: 'Papar senarai command bot',
+  async execute({ msg, config }) {
+    const menuText = `🧾 *${config.botname} Menu:*
 
+👑 *Admin Only*
+• .addpremium [nombor]
+• .delpremium [nombor]
+• .self / .public
+
+💎 *Premium Only*
+• .aiimage [teks]
+• .hd (reply gambar)
+• .brat (reply gambar)
+
+📦 *Umum*
+• .menu
+• .tagall
+• .hidetag [teks]
+• .sticker (reply gambar)
+• .runtime
+• .ping
+
+📌 *Status:*
+• Self Mode: ${config.selfMode ? '✅ ON' : '❌ OFF'}
+• Premium User: ${msg.from.split('@')[0]}
+`;
+
+    msg.reply(menuText);
+  }
+};
