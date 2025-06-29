f15cc98fbcd7409483c31db62c818fcb03c08260
syuhada05 <addastorefollower@gmail.com>
2025-06-30 01:47:56 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,14 @@
+module.exports = {
+  name: 'ping',
+  description: 'Check bot response time / Semak kelajuan bot',
+  async execute({ msg, sock }) {
+    const start = Date.now();
+    const sentMsg = await msg.reply('🏓 Pinging...');
+    const end = Date.now();
+    const ping = end - start;
 
+    await sock.sendMessage(msg.from, {
+      text: `🏓 *Pong!*\n⏱️ *Speed:* ${ping} ms\n📶 *Bot is active & responsive!*`,
+    }, { quoted: sentMsg });
+  }
+};
